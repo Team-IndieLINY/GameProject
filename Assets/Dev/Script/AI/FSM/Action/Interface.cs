1450d72be7c2d93f275321f717433cae435ad4d8
ahhjm881 <ahhjm881@naver.com>
2024-04-03 08:04:13 +0900

    :sparkles: FSM 볼트 연계
    변경사항:
    - bolt의 State Machine과 연계해서 FSM 기반을 제작중에 있음
    
    코멘트:
    - Param 데이터를 transition에서 설정할 수 있는 기반 제작해야함

---
+++ Assets/Dev/Script/AI/FSM/Action/Interface.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace IndieLINY.AI
+{
+    public interface IParam
+    {
+        
+    }
+
+    public interface IAction
+    {
+        public void SetParam(IParam param);
+        public IParam GetParam();
+    }
+
+    public abstract class BaseAction : ScriptableObject, IAction
+    {
+        [CanBeNull] internal IParam _param;
+        
+        public void SetParam([CanBeNull] IParam param)
+        {
+            _param = param;
+        }
+
+        [CanBeNull]
+        public IParam GetParam()
+        {
+            return _param;
+        }
+    }
+    public abstract class BaseActionT<TParam> : BaseAction
+        where TParam : class, IParam
+    {
+        public TParam Param => _param as TParam;
+    }
+}
