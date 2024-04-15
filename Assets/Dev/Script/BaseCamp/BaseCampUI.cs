9ef4991a711f2639b0ef682dbdbead753f3229be
juho Kim <42205951+gabjuho@users.noreply.github.com>
2024-04-16 05:47:09 +0900

    :sparkles: 베이스 캠프 씬 추가

---
+++ Assets/Dev/Script/BaseCamp/BaseCampUI.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class BaseCampUI : MonoBehaviour
+{
+    private UIDocument _uiDocument;
+    private VisualElement _rootVisualElement;
+    private VisualElement _containerVisualElement;
+    private Button _button;
+
+    private void Awake()
+    {
+        _uiDocument = GetComponent<UIDocument>();
+        _rootVisualElement = _uiDocument.rootVisualElement;
+        _containerVisualElement = _rootVisualElement.Q<VisualElement>("Container");
+        _button = _containerVisualElement.Q<Button>("Button");
+        _button.RegisterCallback<ClickEvent>(LoadOutSideScene);
+    }
+
+    public void LoadOutSideScene(ClickEvent evt)
+    {
+        SceneManager.LoadScene("hapchigi");
+    }
+}
